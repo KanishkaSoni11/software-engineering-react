82401376de09e72136d3ef52dc646a133dd35b3f
KanishkaSoni11 <90636506+KanishkaSoni11@users.noreply.github.com>
2022-03-19 00:51:12 -0400

    before

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,15 +1,161 @@
-describe('can create tuit with REST API', () => {
-  // TODO: implement this
-});
+import {
+    createTuit,
+    deleteTuit, findTuitById,
+    findAllTuits,
+} from "../services/tuits-service";
 
+import {
+    createUser, deleteUsersByUsername
+} from "../services/users-service";
+
+describe('createTuit',  () => {
+    const ripley = {
+        username: 'ellenripley',
+        password: 'lv426',
+        email: 'ellenripley@aliens.com'
+    };
+
+
+    const tuitRipley = {
+        tuit: 'Hi Ripley',
+    };
+
+    // setup test before running test
+    beforeAll(() => {
+        // remove any/all users to make sure we create it in the test
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    // clean up after test runs
+    afterAll(() => {
+        // remove any data we created
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    test('can create tuit with REST API', async () => {
+        const newUser = await createUser(ripley);
+        const newTuit = await createTuit(newUser._id, tuitRipley);
+        expect(newTuit.tuit).toEqual(tuitRipley.tuit);
+        expect(newTuit.postedBy).toEqual(newUser._id);
+
+    })
+});
 describe('can delete tuit wtih REST API', () => {
-  // TODO: implement this
+    const ripley = {
+        username: 'ellenripley',
+        password: 'lv426',
+        email: 'ellenripley@aliens.com'
+    };
+
+    const tuitRipley = {
+        tuit: 'Hi Ripley',
+    };
+
+    // setup test before running test
+    beforeAll(async () => {
+        // insert the sample user we then try to remove
+        //const newUser = await createUser(ripley);
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    // clean up after test runs
+    afterAll(() => {
+        // remove any data we created
+        return deleteUsersByUsername(ripley.username);
+    })
+    test('can delete tuit wtih REST API',async() => {
+        const newUser = await createUser(ripley);
+        const newTuit = await createTuit(newUser._id, tuitRipley);
+        const status = await deleteTuit(newTuit._id);
+        expect(status.deletedCount).toBeGreaterThanOrEqual(1);
+    })
 });
 
 describe('can retrieve a tuit by their primary key with REST API', () => {
-  // TODO: implement this
+    const ripley = {
+        username: 'ellenripley',
+        password: 'lv426',
+        email: 'ellenripley@aliens.com'
+    };
+
+    const tuitRipley = {
+        tuit: 'Hi Ripley',
+    };
+
+    // setup test before running test
+    beforeAll(async () => {
+        // insert the sample user we then try to remove
+        //const newUser = await createUser(ripley);
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    // clean up after test runs
+    afterAll(() => {
+        // remove any data we created
+        return deleteUsersByUsername(ripley.username);
+    })
+    test('can retrieve a tuit by their primary key with REST API',async() => {
+        const newUser = await createUser(ripley);
+        const newTuit = await createTuit(newUser._id, tuitRipley);
+
+        expect(newTuit.tuit).toEqual(tuitRipley.tuit);
+        expect(newTuit.postedBy).toEqual(newUser._id);
+
+        const existingTuit = await findTuitById(newTuit._id);
+
+        expect(existingTuit.tuit).toEqual(tuitRipley.tuit);
+        expect(existingTuit.postedBy._id).toEqual(newUser._id)
+    })
 });
 
 describe('can retrieve all tuits with REST API', () => {
-  // TODO: implement this
+    const ripley = {
+        username: 'ellenripley',
+        password: 'lv426',
+        email: 'ellenripley@aliens.com'
+    };
+
+    const tuits= [
+        {
+            tuit : "Hello1"
+        },
+        {
+            tuit : "Hello2"
+        },
+        {
+            tuit : "Hello3"
+        },
+    ]
+
+    beforeAll(async () => {
+ //       await Promise.all(tuits.map(async (tuit) => await deleteTuit(tuit._id)));
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    afterAll(async () => {
+        //await Promise.all(tuits.map(async (tuit) => await deleteTuit(tuit._id)));
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    test('can retrieve all tuits with REST API',async() => {
+        const newUser = await createUser(ripley);
+        tuits.map(tuit =>
+        createTuit(
+            newUser._id,
+            tuit
+        ))
+
+        const tuit1 = await findAllTuits();
+        expect(tuit1.length).toBeGreaterThanOrEqual(tuits.length);
+
+
+        const tuitWeInserted = tuits.filter(
+            tuit => tuits.indexOf(tuit.tuit) >= 0);
+
+        tuitWeInserted.forEach(tuit1 => {
+            const tuit =  tuits.find(tuit => tuit === tuit.tuit);
+            expect(tuit.tuit).toEqual(tuitWeInserted);
+            expect(tuit.postedBy).toEqual(tuitWeInserted);
+        })
+    })
 });
--- src/tests/user-list.test.js
+++ src/tests/user-list.test.js
@@ -3,9 +3,8 @@ import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllUsers} from "../services/users-service";
 import axios from "axios";
-import {createUser} from "./services";
+//jest.mock('axios');
 
-jest.mock('axios');
 
 const MOCKED_USERS = [
   {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
@@ -14,9 +13,9 @@ const MOCKED_USERS = [
 
 test('user list renders static user array', () => {
   render(
-    <HashRouter>
-      <UserList users={MOCKED_USERS}/>
-    </HashRouter>);
+      <HashRouter>
+        <UserList users={MOCKED_USERS}/>
+      </HashRouter>);
   const linkElement = screen.getByText(/ellen_ripley/i);
   expect(linkElement).toBeInTheDocument();
 });
@@ -24,25 +23,31 @@ test('user list renders static user array', () => {
 test('user list renders async', async () => {
   const users = await findAllUsers();
   render(
-    <HashRouter>
-      <UserList users={users}/>
-    </HashRouter>);
+      <HashRouter>
+        <UserList users={users}/>
+      </HashRouter>);
   const linkElement = screen.getByText(/alice/i);
   expect(linkElement).toBeInTheDocument();
 })
 
 test('user list renders mocked', async () => {
-  axios.get.mockImplementation(() =>
-    Promise.resolve({ data: {users: MOCKED_USERS} }));
-
+  const mock = jest.spyOn(axios, 'get');
+  mock.mockImplementation(() => Promise.resolve({data: {users: MOCKED_USERS}}));
   const response = await findAllUsers();
   const users = response.users;
-
+  mock.mockRestore();
   render(
-    <HashRouter>
-      <UserList users={users}/>
-    </HashRouter>);
+      <HashRouter>
+        <UserList users={users}/>
+      </HashRouter>);
 
   const user = screen.getByText(/ellen_ripley/i);
   expect(user).toBeInTheDocument();
+
+  mock.mockRestore();
 });
+
+
+
+
+
